459a1acb5761e657b19dfe3828ab9b495d219e90
krister-henriksson <krister.henriksson@outlook.com>
2023-04-28 06:14:07 +0000

    dev

--- Ch01/01_04/duplicates.py
+++ Ch01/01_04/duplicates.py
@@ -5,8 +5,19 @@ import pandas as pd
 # %%
 df = pd.read_csv('cart.csv', parse_dates=['date'])
 df
+
 # %%
 df.duplicated()
+
+# %%
+# df['date', 'name']
+type(df[['date', 'name']])
+
+# %%
+type(df['date'])
+
+# %%
+type(df[['date']])
 
 # %%
 df.duplicated(['date', 'name'])

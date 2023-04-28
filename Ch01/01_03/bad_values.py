768c74697e9c2b1e4fb9fbff26c45d9a43337a50
krister-henriksson <krister.henriksson@outlook.com>
2023-04-28 06:00:05 +0000

    dev

--- Ch01/01_03/bad_values.py
+++ Ch01/01_03/bad_values.py
@@ -12,14 +12,45 @@ df.groupby('name').describe()
 df['name'].value_counts()
 
 # %%
+pd.pivot(df, index='time', columns='name')
+
+# %%
 pd.pivot(df, index='time', columns='name').plot(subplots=True)
 
 # %%
 df.query('name == "cpu" & (value < 0 | value > 100)')
 
+# %%
+mem = df[df['name'] == 'mem']
+mem.head()
+
 # %% 
+mem = df[df['name'] == 'mem'][['time','value']]
+mem.head()
+
+
+# %%
+# Actually a Series:
+mem2 = df[df['name'] == 'mem'][['value']]
+mem2.head()
+
+
+
+# %%
 mem = df[df['name'] == 'mem']['value']
+mem.head()
+
+
+# %%
 z_score = (mem - mem.mean())/mem.std()
+z_score
+
+# %%
 bad_mem = mem[z_score.abs() > 2]
+bad_mem
+
+# %%
 # bad_mem
 df.loc[bad_mem.index]
+
+# %%
